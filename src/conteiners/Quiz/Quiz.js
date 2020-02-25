291d32de5edd7da8757e976dcaaaaaf51a26b94e
Kate Fenchenko <fev3@mail.ru>
2020-02-25 09:11:55 +0300

    redux create quiz page

---
+++ src/conteiners/Quiz/Quiz.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react'
+import classes from './Quiz.css'
+import ActiveQuiz from './../../components/ActiveQuiz/ActiveQuiz'
+import FinishedQuiz from './../../components/FinishedQuiz/FinishedQuiz'
+// import axios from '../../axios/axios-quiz'
+import Loader from '../../components/UI/Loader/Loader'
+import {connect} from 'react-redux'
+import {fetchQuizById, quizAnswerClick, retryQuiz} from '../../store/actions/quiz'
+
+
+class Quiz extends Component {
+        /* isQuizFinished() {
+        return this.state.activeQuestion+1 === this.state.quiz.length
+    }      */
+    
+    /* retryHandler = () => {
+        this.setState({
+            results: {},
+            isFinished: false,
+            answerState: null,
+            activeQuestion: 0,
+        })
+    } */
+    componentDidMount() {
+        this.props.fetchQuizById(this.props.match.params.id)
+    }
+
+    componentWillUnmount() {
+        this.props.retryQuiz()
+    }
+
+    render() {
+        return (
+            <div className = {classes.Quiz}>
+                
+                <div className = {classes.QuizWrapper}>
+                    <h1>Ответьте на все вопросы</h1>
+                    {
+                        this.props.loading || !this.props.quiz
+                            ? <Loader />
+                            :  this.props.isFinished 
+                            ? <FinishedQuiz 
+                                results = {this.props.results}
+                                quiz = {this.props.quiz}
+                                onRetry = {this.props.retryQuiz}
+                              />
+                            : <ActiveQuiz 
+                                answers = {this.props.quiz[this.props.activeQuestion].answers}
+                                question = {this.props.quiz[this.props.activeQuestion].question}
+                                onAnswerClick = {this.props.quizAnswerClick}
+                                quizLength = {this.props.quiz.length}
+                                answerNumber = {this.props.activeQuestion+1}
+                                state = {this.props.answerState}
+                            />
+                    
+                       
+                    }
+                    
+                        
+                    
+
+                    
+                </div>
+
+            </div>
+        )
+
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        results: state.quiz.results,
+        isFinished: state.quiz.isFinished,
+        answerState: state.quiz.answerState,
+        activeQuestion: state.quiz.activeQuestion,
+        quiz: state.quiz.quiz,
+        loading: state.quiz.loading
+
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchQuizById: id => dispatch(fetchQuizById(id)),
+        quizAnswerClick: answerId => dispatch(quizAnswerClick(answerId)),
+        retryQuiz: () => dispatch(retryQuiz())
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Quiz)
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import {combineReducers}  from 'redux'
+import quizReducer from './quiz'
+import createReducer from './create'
+
+
+export default combineReducers({
+    quiz: quizReducer,
+    create: createReducer
+})
